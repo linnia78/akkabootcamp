665653c74bec22a51fb427924e1942edf95795ba
Allen Chen <Allen@INSELLERATE.LOCAL>
2016-09-02 14:44:12 -0700

    update bootcamp

---
+++ src/Unit-1/DoThis/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+﻿using Akka.Actor;
+
+namespace WinTail
+{
+    #region Program
+    class Program
+    {
+        public static ActorSystem MyActorSystem;
+
+        static void Main(string[] args)
+        {
+            // initialize MyActorSystem
+            MyActorSystem = ActorSystem.Create("TheSystem");
+
+            PrintInstructions();
+
+            // time to make your first actors!
+            //YOU NEED TO FILL IN HERE
+            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
+            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            ////var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+            ////var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
+
+            //not recommended
+            Props consoleWriterProps = Props.Create(typeof(ConsoleWriterActor));
+            //recommended
+            Props consoleWriterProps3 = Props.Create<ConsoleWriterActor>();
+
+            Props consoleWriterProps2 = Props.Create(() => new ConsoleWriterActor());
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps2, "consoleWriterActor");
+
+            Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+
+            Props consoleReaderProps = Props.Create(() => new ConsoleReaderActor(validationActor));
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+            // tell console reader to begin
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.Write("Some lines will appear as");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(" red ");
+            Console.ResetColor();
+            Console.Write(" and others will appear as");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" green! ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+    }
+    #endregion
+}
